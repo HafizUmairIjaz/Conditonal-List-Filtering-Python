8c866f9b34c6a6b27150dcda2d3f1a1086fb3700
Hafiz Umair Ijaz <hafizumairijaz@gmail.com>
2023-09-22 01:26:46 +0500

    conditonal list filtering

---
+++ 7.py
@@ -0,0 +1,16 @@
+name=['Amina','Lubna ', ' Aliza ','Amir']
+
+check= 'A'
+
+res = [idx for idx in name if idx[0].lower() == check.lower()]
+
+
+
+print("The list of matching first letter : " + str(res))
+
+
+
+
+
+
+
